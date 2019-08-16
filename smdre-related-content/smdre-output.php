37b4fb9da5d4a2e64ea82474a16e06c3a10a948c
davideC00 <dvdx97@gmail.com>
2019-08-16 15:39:39 +0200

    Bug fix
    metatags was not showns in front-page

--- smdre-related-content/smdre-output.php
+++ smdre-related-content/smdre-output.php
@@ -25,6 +25,10 @@ function smdre_print_tags (){
   //get the id of the current post
   $post_id = get_the_ID();
 
+  if(is_front_page()){
+    // in this case get_the_ID doesn't return the correct ID for the front-page
+    $post_id = smdre_get_site_meta_id();
+  }
 
   $html = "";
 
@@ -36,5 +40,29 @@ function smdre_print_tags (){
 
 
 
+/**
+ * A function that returns all the metadata from the site_meta cpt
+ * This is like when we use pressbooks to gather all data from Book Info
+ * We are always working on a single post -- automatic
+ * This function will be mostly used when the plugin is on wordpress mode and not on pressbooks mode.
+ */
+function smdre_get_site_meta_id(){
+  $post_type = is_plugin_active ('pressbooks/pressbooks.php') ? 'metadata' : 'site-meta';
+  $args = array(
+    'post_type'      => $post_type,
+    'posts_per_page' => 1,
+    'post_status'    => 'publish',
+    'orderby'        => 'modified',
+    'no_found_rows'  => true,
+    'cache_results'  => true,
+  );
 
+  $db = new \WP_Query();
+  $results = $db->query( $args );
+
+  if ( empty( $results ) ) {
+    return false;
+  }
+
+  return $results[0]->ID;
 }
